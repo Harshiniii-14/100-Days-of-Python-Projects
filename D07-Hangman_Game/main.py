1401a08a4a717f02b22a4618700feb5b878836d3
Patalam Harshini <147309597+Harshiniii-14@users.noreply.github.com>
2026-01-07 22:11:36 +0530

    Add files via upload

---
+++ D07-Hangman_Game/main.py
@@ -0,0 +1,61 @@
+import random
+import hangman_words
+import hangman_art
+
+
+word_list = hangman_words.word_list
+lives = 6
+stages = hangman_art.stages
+
+print(hangman_art.logo)
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
+
+game_over = False
+correct_letters = []
+guessed = []
+
+while not game_over:
+
+    print(f"****************************{lives} LIVES LEFT****************************")
+    guess = input("Guess a letter: ").lower()
+
+
+    if guess in guessed:
+        print(f"You've already guessed {guess}")
+        continue
+
+    guessed.append(guess)
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        print(stages[lives])
+        if lives == 0:
+            game_over = True
+
+            print(f"IT WAS <{chosen_word}>! YOU LOSE")
+
+    if "_" not in display:
+        game_over = True
+        print("****************************YOU WIN****************************")
+
